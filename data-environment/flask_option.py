ba1adc1a6cef9ec1c9332da31f88d3c22d4cc6fb
Elif <elifcal01@gmail.com>
2026-03-12 00:49:31 +0300

    Add start function to handle FLASK_ENV variable

--- data-environment/flask_option.py
+++ data-environment/flask_option.py
@@ -3,8 +3,16 @@
 import os
 
 def start():
-    """returns the right message"""
-    pass  # YOUR CODE HERE
+    env_value = os.getenv("FLASK_ENV")
+
+    if env_value == "development":
+        return "Starting in development mode..."
+
+    elif env_value == "production":
+        return "Starting in production mode..."
+
+    else:
+        return "Starting in empty mode..."
 
 if __name__ == "__main__":
     print(start())
