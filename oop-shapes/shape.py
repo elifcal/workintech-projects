ff84fd193b3c92998cea6387fbe6cfe52d736473
Elif <elifcal01@gmail.com>
2026-03-17 02:02:57 +0300

    implement Shape, Rectangle, and Circle classes

--- oop-shapes/shape.py
+++ oop-shapes/shape.py
@@ -1,2 +1,41 @@
-# pylint: disable=too-few-public-methods missing-module-docstring
-pass  # YOUR CODE HERE
+import math
+
+class Shape:
+
+    def __init__(self, color, name):
+        self.color = color
+        self.name = name
+
+    def say_name(self):
+        return f"My name is {self.name}."
+
+class Rectangle(Shape):
+    def __init__(self, color, name,  width, height):
+
+        super().__init__(color, name)
+        self.width =  width
+        self.height = height
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        return 2 * (self.height + self.width)
+
+    def say_name(self):
+        return f"My name is {self.name} and I am a rectangle."
+
+class Circle(Shape):
+    def __init__(self, color, name, radius):
+
+        super().__init__(color, name)
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+    def say_name(self):
+        return f"My name is {self.name} and I am a circle."
